14c3d1596a4535ede98222a6c910bf7b5c3a993c
chethankarthikeya-sundharesh23 <chethankarthikeya.su@ucalgary.ca>
2026-03-12 18:16:55 -0600

    Polyline drawing

--- Lab 6/script.js
+++ Lab 6/script.js
@@ -5,3 +5,44 @@ var map = L.map('map').setView([51.0447, -114.0719], 12);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'OpenStreetMap'
 }).addTo(map);
+
+
+// Feature group to store drawn items
+var drawnItems = new L.FeatureGroup();
+map.addLayer(drawnItems);
+
+
+// Draw control (only allow polyline)
+var drawControl = new L.Control.Draw({
+    draw: {
+        polyline: true,
+        polygon: false,
+        rectangle: false,
+        circle: false,
+        marker: false,
+        circlemarker: false
+    },
+    edit: {
+        featureGroup: drawnItems
+    }
+});
+
+map.addControl(drawControl);
+
+
+// Variable to store the drawn polyline
+var drawnLine = null;
+
+
+// When a polyline is created
+map.on(L.Draw.Event.CREATED, function (event) {
+
+    var layer = event.layer;
+
+    // store the line
+    drawnLine = layer;
+
+    // add to map
+    drawnItems.addLayer(layer);
+
+});
